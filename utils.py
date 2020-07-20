e75516a282f2297f90aaa912337289e7c1abe08b
Yeahia Sarker <yeahiasarkernabil@gmail.com>
2020-07-20 18:47:31 +0600

    add utils.py

---
+++ utils.py
@@ -0,0 +1,4 @@
+def squash(vectors, axis=-1):
+    s_squared_norm = K.sum(K.square(vectors), axis, keepdims=True)
+    scale = s_squared_norm / (1 + s_squared_norm) / K.sqrt(s_squared_norm + K.epsilon())
+    return scale * vectors
